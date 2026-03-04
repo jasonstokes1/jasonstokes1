7081667711a8b19c85a8cda90d37d8826f3c1dbd
Jason  Stokes <jasonpaulstokes@gmail.com>
2026-03-04 16:57:47 -0600

    Create WN11-AC-000045.ps1

---
+++ STIGS/WN11-AC-000045.ps1
@@ -0,0 +1,32 @@
+<#
+.SYNOPSIS
+    This PowerShell script programmatically enforces the DISA STIG requirement WN11-AC-000045 for Windows 11. 
+    It ensures that the system automatically initiates a screen lock after a specific period of inactivity to prevent unauthorized access to an unattended workstation.
+    
+.NOTES
+    Author          : Jason Stokes
+    LinkedIn        : https://www.linkedin.com/in/jason-stokes-50144518a/
+    GitHub          : https://github.com/jasonstokes1
+    Date Created    : 2026-03-02
+    Last Modified   : 2024-03-04
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN11-CC-000315
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\__remediation_template(STIG-ID-WN11-AU-000500).ps1 
+#>
+
+# Set screen saver timeout to 900 seconds (15 minutes)
+$path = "HKCU:\Control Panel\Desktop"
+Set-ItemProperty -Path $path -Name "ScreenSaveTimeOut" -Value "900" -Force
+Write-Host "WN11-AC-000045: Timeout set to 15 minutes." -ForegroundColor Green
